b946ee9fada325be970b1e476fb35206069e9dcd
Mohamed Mohamed <mohamedmohamed@Mohameds-MacBook-Pro-2.local>
2022-09-17 13:56:34 -0500

    First Push

--- Develop/server.js
+++ Develop/server.js
@@ -48,3 +48,11 @@ app.post('/api/notes', (req, res) => {
     res.json(newNote);
 });
 
+app.delete('/api/notes/:id', (req, res) => {
+    deleteNote(req.params.id, allNotes);
+    res.json(true);
+});
+
+app.listen(PORT, () => {
+    console.log(`API server now on port ${PORT}!`);
+});
