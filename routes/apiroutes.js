df5b54d12a9598a7a3f51a0f3467a571386a175a
Mohamed <m.mohamedsandiego@gmail.com>
2022-09-18 15:43:16 -0500

    Notes

--- routes/apiroutes.js
+++ routes/apiroutes.js
@@ -1,33 +1,45 @@
 const app = require('express').Router()
 const fs = require('fs');
-
-const allNotes = require('./db/db.json');
+const path = require('path');
+let allNotes = require('../db/db.json');
 
 
 app.get('/api/notes', (req, res) => {
-    res.json(allNotes.slice(1));
+    allNotes =JSON.parse(fs.readFileSync("./db/db.json"))
+    res.json(allNotes);
 });
 
 
 
 function createNewNote(body, notesArray) {
     const newNote = body;
-    if (!Array.isArray(notesArray))
-        notesArray = [];
-    
-    if (notesArray.length === 0)
-        notesArray.push(0);
-
-    body.id = notesArray[0];
-    notesArray[0]++;
-
-    notesArray.push(newNote);
+   
+     newNote.id = Math.floor(Math.random() * 892)  
+  
+    allNotes.push(newNote);
     fs.writeFileSync(
-        path.join(__dirname, './db/db.json'),
+        path.join(__dirname, '../db/db.json'),
+        JSON.stringify(allNotes))
+    
+    return allNotes;
+}
+
+function deleteNote(id,notesArray){
+    let tempArray = []
+    for(let i=0;i<notesArray.length;i++){
+        if(notesArray[i].id != id){
+            tempArray.push(notesArray[i])
+        }
+    }
+    notesArray = tempArray
+    fs.writeFileSync(
+        path.join(__dirname, '../db/db.json'),
         JSON.stringify(notesArray, null, 2)
     );
-    return newNote;
+    return notesArray;
 }
+
+
 app.post('/api/notes', (req, res) => {
     const newNote = createNewNote(req.body, allNotes);
     res.json(newNote);
